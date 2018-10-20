7d20c254a5016154d34d572e3ee15f93d02e1469
DESKTOP-6UEQ57L\Conan <conny.nyman93@gmail.com>
2018-10-20 15:23:08 +0300

    Added Header component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './styles/css/App.css';
 
+import Header from './components/Header/Header';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>React Loan App</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App container-fluid">
+                <Header title="React Loan App" cssClasses="py-5 bg-light"/>
+            </div>
+        );
+    }
 }
 
 export default App;
